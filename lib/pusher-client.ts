dffa0cd198af445891b30d5dd6cdb7a8084d2fbd
ilias pouriki <eliaspourikis1@gmail.com>
2025-11-25 14:40:03 +0200

    Fix cluster fallback

--- lib/pusher-client.ts
+++ lib/pusher-client.ts
@@ -1,10 +1,8 @@
-// lib/pusher-client.ts
 import Pusher from "pusher-js";
 
 export const pusherClient = new Pusher(
-  process.env.NEXT_PUBLIC_PUSHER_KEY || "",
+  process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "eu",
-    forceTLS: true,
   }
 );
